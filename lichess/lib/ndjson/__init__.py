0376697a04b73d4f4eb399f2b5a5af9fdfdf4718
Roman A <dev@rman.dev>
2025-11-22 18:49:05 +0000

    include ndjson in source

---
+++ lichess/lib/ndjson/__init__.py
@@ -0,0 +1,5 @@
+from .codecs import Decoder, Encoder
+from .api import load, loads, dump, dumps, writer, reader
+
+
+__all__ = ["Decoder", "Encoder", "load", "loads", "dump", "dumps", "writer", "reader"]
